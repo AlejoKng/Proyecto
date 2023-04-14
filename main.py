eb1bcaae79e40d92ccad03f80174af2683b30e20
David Cardenas <alzadavid10@gmail.com>
2023-04-14 15:18:53 -0500

    terminado

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class VentanaPrincipal(QMainWindow):
     def __init__(self):
         super(VentanaPrincipal, self).__init__()
         loadUi('C:\Escritorio\DAVID\VS_Code\Proyecto\Proyecto\mante.ui',self)
+        
 
         self.bt_menu.clicked.connect(self.mover_menu)
 
@@ -19,24 +20,21 @@ class VentanaPrincipal(QMainWindow):
         self.bt_colapsar.clicked.connect(self.control_bt_normal)
         self.bt_cerrar.clicked.connect(lambda: self.close())
     
-        self.bt_database.clicked.connect(self.page_base_datos)
-        self.bt_registrar.clicked.connect(self.registrar_mantenimiento)
-        self.bt_actualizar.clicked.connect(self.modificar_mantenimiento)
-        self.bt_eliminar.clicked.connect(self.eliminar_mantenimiento)
-        self.bt_program.clicked.connect(self.programacion_mantenimiento)
-        self.bt_pgElim_consul.clicked.connect(self.buscar_OT_eliminar)
-        self.bt_pgElim_save.clicked.connect(self.actualiza_OT_eliminar)
-        self.bt_pgAct_consul.clicked.connect(self.buscar_OT_modificar)
-        self.bt_pgAct_save.clicked.connect(self.actualizar_OT_modificar)
-        self.bt_pgReg_save.clicked.connect(self.guardar_OT)
-        self.bt_pgBD_refrescar.clicked.connect(self.actualizar_OT)
+        # self.bt_database.clicked.connect(self.page_base_datos)
+        # self.bt_registrar.clicked.connect(self.registrar_mantenimiento)
+        # self.bt_actualizar.clicked.connect(self.modificar_mantenimiento)
+        # self.bt_eliminar.clicked.connect(self.eliminar_mantenimiento)
+        # self.bt_program.clicked.connect(self.programacion_mantenimiento)
+        # self.bt_pgElim_consul.clicked.connect(self.buscar_OT_eliminar)
+        # self.bt_pgElim_save.clicked.connect(self.actualiza_OT_eliminar)
+        # self.bt_pgAct_consul.clicked.connect(self.buscar_OT_modificar)
+        # self.bt_pgAct_save.clicked.connect(self.actualizar_OT_modificar)
+        # self.bt_pgReg_save.clicked.connect(self.guardar_OT)
+        # self.bt_pgBD_refrescar.clicked.connect(self.actualizar_OT)
         #Eliminar barra de titulo y opacidad
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
         self.setWindowOpacity(1)
-        #SizeGrip
-        self.gripSize=10
-        self.grip=QtWidgets.QSizeGrip(self)
-        self.grip.resize(self.gripSize,self.gripSize)
+
 
         #Mover ventana
         self.frame_superior.mouseMoveEvent = self.mover_ventana
@@ -52,8 +50,14 @@ class VentanaPrincipal(QMainWindow):
         self.tabla_OT_pgElim.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.tabla_OT_pgBD.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
+        
+        #SizeGrip
+        self.gripSize=10
+        self.grip=QtWidgets.QSizeGrip(self)
+        self.grip.resize(self.gripSize,self.gripSize)
     def control_bt_minimizar(self):
         self.showMinimized()
+
     def control_bt_normal(self):
         self.showNormal()
     def control_bt_maximizar(self):
@@ -105,11 +109,9 @@ class VentanaPrincipal(QMainWindow):
             cantidad =self.reg_cantidad.clear()
         else:
             self.signal_registrar.setText('Hay Espacios Vacios')
+
 if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = VentanaPrincipal()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    app = QApplication(sys.argv)
+    mi_app = VentanaPrincipal()
+    mi_app.show()
     sys.exit(app.exec_())
